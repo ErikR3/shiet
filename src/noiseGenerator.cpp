2f1c56cadf36c051660a6f58dbdb16de86336750
ErikR3 <erik.regner6@gmail.com>
2024-04-14 13:19:28 +0200

    q

--- src/noiseGenerator.cpp
+++ src/noiseGenerator.cpp
@@ -1,4 +1,24 @@
 #include "noiseGenerator.h"
 
-noiseGenerator::noiseGenerator
+noiseGenerator::noiseGenerator()
+{
+    FastNoiseLite noise = FastNoiseLite();
+
+    noise.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
+    noise.SetSeed(69);
+}
+
+void noiseGenerator::startNoise(float x, float y)
+{
+    float coords[16][16];
+    for (int i = 0; i < 16; i++)
+    {
+        for (int j = 0; j < 16; j++)
+        {
+            
+        }
+    }
+    
+    
+}
 
--- src/noiseGenerator.h
+++ src/noiseGenerator.h
@@ -1,12 +1,16 @@
 #ifndef NOISEGENERATOR_H
 #define NOISEGENERATOR_H
 #include <FastNoiseLite.h>
+#include <vector>
 
 
     class noiseGenerator
     {
         public:
-            noiseGenerator(float x, float y);
+            FastNoiseLite noise = FastNoiseLite();
+            noiseGenerator();
+            void startNoise(float x, float y);
+            float generateNoise(float x, float y);
     };
 
 #endif
