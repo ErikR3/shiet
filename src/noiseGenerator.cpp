1488219ef0ff9fb157aaf9c37505161826976012
ErikR3 <erik.regner6@gmail.com>
2024-04-15 11:44:59 +0200

    qqq

--- src/noiseGenerator.cpp
+++ src/noiseGenerator.cpp
@@ -2,20 +2,18 @@
 
 noiseGenerator::noiseGenerator()
 {
-    FastNoiseLite noise = FastNoiseLite();
-
     noise.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
     noise.SetSeed(69);
 }
 
 void noiseGenerator::startNoise(float x, float y)
 {
-    float coords[16][16];
+    float c[16][16];
     for (int i = 0; i < 16; i++)
     {
         for (int j = 0; j < 16; j++)
         {
-            
+            c[i][j] = noise.GetNoise(x, y);
         }
     }
     
